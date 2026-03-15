871a56b8f45fb142552b8c8786afac199558e357
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-03-15 17:23:56 -0300

    Criando css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TaskFlow</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 <body>
     
     <h1>TaskFlow</h1>
 
-    <a href="{{ route('tasks.index') }}">Lista de Tarefas</a>
+    <nav>
+        <a href="{{ route('tasks.index') }}" class="btn">
+            Lista de Tarefas
+        </a>
+    </nav>
 
 </body>
 </html>
